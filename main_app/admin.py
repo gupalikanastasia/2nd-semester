b3b11323f2989ca906f3d7c637f48fefd0510073
Anastasia <3979705406@tac.lutsk.ua>
2026-03-08 01:47:23 +0200

    Lab 4: making database for the site

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, MenuItem, Location
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at')
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'created_at', 'updated_at')
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'created_at', 'updated_at')
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Назва категорії")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Назва страви")
+    description = models.TextField(verbose_name="Опис")
+    price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name="Ціна")
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='items')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Назва закладу")
+    address = models.CharField(max_length=255, verbose_name="Адреса")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name

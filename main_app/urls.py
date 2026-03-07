2852f9fecce0be65a68262e12a66dd71c7313f5c
Anastasia <3979705406@tac.lutsk.ua>
2026-03-08 00:58:44 +0200

    Lab 3: Changed the structure of site to cafeteria

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('somerset/', views.somerset, name='somerset'),
-    path('dcip/', views.dcip, name='dcip'),
-    path('coffee/', views.bean_water, name='bean_water'),
+    path('coffee/', views.coffee, name='coffee'),
+    path('cakes/', views.cakes, name='cakes'),
+    path('desserts/', views.desserts, name='desserts'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,32 +2,32 @@ from django.shortcuts import render
 
 def home(request):
     context = {
-        'title': 'Головна: Наші заклади у Луцьку',
-        'content': 'Вітаємо! Оберіть заклад, щоб побачити опис для соцмереж.',
+        'title': 'Головна: Sleepy Pandy',
+        'content': 'Вітаємо! Оберіть категорію, щоб зробити замовлення',
         'page_type': 'home'
     }
     return render(request, 'main_app/index.html', context)
 
-def somerset(request):
+def coffee(request):
     context = {
-        'title': 'Somerset Hotel',
-        'content': 'Затишний, озеленений готель для сімейного відпочинку. Ідеальне місце для спокою в центрі міста.',
+        'title': 'Кава',
+        'content': 'Смачна, запашна кава яка підійме ваш настрі в найпохмуріший ранок.',
         'page_type': 'detail'
     }
     return render(request, 'main_app/index.html', context)
 
-def dcip(request):
+def cakes(request):
     context = {
-        'title': 'DCIP (Don’t Call It Pizza)',
-        'content': 'Елегантна та вишукана піцерія. Це не просто піца, це гастрономічний досвід.',
+        'title': 'Авторська випічка',
+        'content': 'Елегантна та вишукана випічка, що зачарує вас своїм смаком',
         'page_type': 'detail'
     }
     return render(request, 'main_app/index.html', context)
 
-def bean_water(request):
+def desserts(request):
     context = {
-        'title': 'Bean and Water',
-        'content': 'Кав’ярня з дуже затишною та м’якою атмосферою. Найкраща кава для твого ранку.',
+        'title': 'Десерти',
+        'content': 'Солодкі та цікаві десерти яких ви ще точно не куштували',
         'page_type': 'detail'
     }
     return render(request, 'main_app/index.html', context)

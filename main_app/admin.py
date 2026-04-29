6ddd18f76e6494fa132019d9274226f94e4ee4fd
Анастасія Гупалік <3979705406@tac.lutsk.ua>
2026-04-29 09:36:26 +0300

    Lab 8:

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, MenuItem, Location
+from .models import Category, MenuItem, Location, Subscription, Rating, Order, OrderItem
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -14,3 +14,23 @@ class MenuItemAdmin(admin.ModelAdmin):
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
     list_display = ('name', 'address', 'created_at', 'updated_at')
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('email', 'created_at') # Бачимо імейли підписників
+    search_fields = ('email',)
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('item', 'score', 'created_at')
+    list_filter = ('score', 'item')
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0 # Щоб не було порожніх рядків
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'total_price', 'created_at')
+    list_filter = ('created_at', 'user')
+    inlines = [OrderItemInline] # Показує товари прямо всередині замовлення
